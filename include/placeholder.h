20566d2dc821be3f9ab60f17f5023fb69e5cf1e2
chadd williams <will4614@pacificu.edu>
2022-05-31 22:03:34 -0700

    Initial commit

---
+++ include/placeholder.h
@@ -0,0 +1,13 @@
+//***************************************************************************
+// File name:   main.cpp
+// Author:      
+// Date:        
+// Class:       
+// Assignment:  
+// Purpose:     
+//***************************************************************************
+
+// this file is a placeholder to keep make happy
+// if you add your own .h file you may remove this file.
+
+#pragma once
